7a2f1c3bdbe346beed1d75c5b0a4c11fc571e436
Indy <ivo.indergand@uzh.ch>
2020-05-15 00:27:37 +0200

    added generator function

--- TDH based on DDH/TDH_functions.py
+++ TDH based on DDH/TDH_functions.py
@@ -1,20 +1,47 @@
 from random import randint
+import random
 import numpy as np
 #https://stackoverflow.com/questions/27784465/how-to-randomly-get-0-or-1-every-time
-def getMatrix (n):
 
-    matrix = (
-        [str(randint(0, 1)) for _ in range(0, n)] for _ in range(0, 2)
-    )
+# Sieve of Eratosthenes to generate primes (if needed)
+# Code by David Eppstein, UC Irvine, 28 Feb 2002
+# http://code.activestate.com/recipes/117119/
+
+def getGenerator(q):
+    q = q
+    g = random.randint(-1000,1000)
+    if g**((q - 1) / 2) != 1%q:
+        return g
+    else:
+        getGenerator(q)
+
+def getBinaryMatrix(n):
+    matrix = np.array([[str(randint(0, 1)) for _ in range(0, n)] for _ in range(0, 2)]).astype(np.int)
     #for row in matrix:
      #   print("".join(row))
 
     return matrix
 
 def hashkey(A,n):
-    n = n
-    hk = 1
-    for row in A:
-        for column in A:
-            hk = A[row][column]*hk
+    #hk = np.prod(A)
+    hk = []
+    y=0
+    while y != n-1:
+            if A[0][y] == 0:
+               hk.append(0)
+            if A[1][y] == 1:
+               hk.append(1)
+            y += 1
+
     return hk
+
+def key_matrix(n,A,s,t):
+    i = random.randint(0,1)            #i remains private given the encoding key
+    matrix = np.zeros((2,n))
+    for row in matrix:
+        for col in matrix:
+            if row == i and col == 1:
+                matrix[row][col] = (A[row][col])**s * A**t
+            else:
+                matrix[row][col] = (A[row][col])**s
+    return matrix
--- TDH based on DDH/TDH_implementation.py
+++ TDH based on DDH/TDH_implementation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from TDH_functions import *
+import matplotlib.pyplot
 
 #Goal: Implement the Trapdoor hash function from DDH assumption
 
@@ -8,16 +9,25 @@ from TDH_functions import *
 
 ##4.2.1“basic” construction of a rate-1/λ TDH scheme for index predicates
 #S(1^lambda,1^n):
+
+#1 1. Sample (G; p; g); Multiplicative abelian group G of prime order p, with a public generator g
+q = 3 #can we take any prime?
+g = getGenerator(q)
 #2. Sample a matrix A
 n = 10
-A = getMatrix(n)
+A = getBinaryMatrix(n)
+print(A)
 #3.Create hash key output; Pillar sign means Product from i=1 to n or over all elements i in set I
-for row in A:
-     print("".join(row))
-
 hk = hashkey(A,n)
 print(hk)
 
+#G(hk,fi)
+#1.s;trapdoor: uniform integer t in Zp
+s = random.randint(-1000,1000)
+t = random.randint(-1000,1000)
+#2.
+u = g**s
+print(key_matrix(n,A,s,t))
 
 
 ##4.2.2.Augmentation to rate-1 TDH in the expense of a λ1 error probability
