5cc13991e6c0480ff9d8573244f20c59eeb3fb6a
Indy <ivo.indergand@uzh.ch>
2020-05-30 16:03:09 +0200

    Closes #13

--- TDH based on DDH/TDH_with_rate1_tds.py
+++ TDH based on DDH/TDH_with_rate1_tds.py
@@ -1,10 +1,11 @@
 import numpy as np
 import random
 
+from pip._vendor.distlib.compat import raw_input
+
 from TDH_functions import *
 from groups import Group #https://github.com/Smeths/pygroup
-import hashlib
-
+from scipy.linalg import norm
 #Goal: Implement the Trapdoor hash function from DDH assumption
 
 #the trapdoor hash function consists only from the Key generation, Encoding and Decoding part
@@ -12,13 +13,14 @@ import hashlib
 class TDH(object):
     #4.2. TDH for Index Predicates from DDH
     ##4.2.1“basic” construction of a rate-1/λ TDH scheme for index predicates
-    def __init__(self,lambda_var,n):
+    def __init__(self,lambda_var,n,rate_1):
         self.lambda_var = lambda_var #the higher lambda the less predictable the bits
         self.n = n
+        self.rate_1 = rate_1
 
     #S for Sampling Algorithm with inputs security parameter lambda and input length n (1^lambda,1^n)
     #..and outputing as hash key hk
-    def sampling_algorithm(self,lambda_var,n): # done by the sender, output also private and only used in the hashing_algorithm again by the sender
+    def sampling_algorithm(self): # done by the sender, output also private and only used in the hashing_algorithm again by the sender
         #1. Sample (G; p; g); (Finite-) multiplicative abelian group G of prime order p, with a public generator g
         G = Group("mult", 9) #group object
         Gg = G.g() #Group list representation
@@ -40,7 +42,7 @@ class TDH(object):
         return hashkey
 
     #G for generating algorithm takes as inputs a hash key hk and a predicate f element of Fn (predicate) (hk,fi) and outputs a pair of an encoding key ek and trapdoor td
-    def generating_algorithm(self,sampling_algorithm): # done by the receiver; encoding_key be made public, trapdoor value must be kept private by the receiver
+    def generating_algorithm(self,sampling_algorithm,lambda_var,n): # done by the receiver; encoding_key be made public, trapdoor value must be kept private by the receiver
         #1.s;trapdoor: uniform integer t in Zp
         #g generates group Zp with g**k mod p with values between 1 and p-1 --s,t both element of Zp
         (G,p,g),A,r = sampling_algorithm
@@ -66,21 +68,22 @@ class TDH(object):
 
     #E(ek,x;p) -- Hinting -- The encoding algorithm takes as input an encoding key ek, string x element of {0,1}**n as well as randomness p_dense elemnt of {0,1}* as input.
     #..and deterministically outputs an encoding e element of {0,1}**w.
-    def encoding_algorithm(self,generating_algorithm,sampling_algorithm,n): #encoding done by sender
+    def encoding_algorithm(self,generating_algorithm,sampling_algorithm,n,rate_1): #encoding done by sender
         (G,p,g),A,r = sampling_algorithm
         (u,B,t,K) = generating_algorithm[0]
         p = int(p)
         g_t = g**t
-        h = hash_value_paper(B,u,r,n) # taken only x parts of key_nr => is that the whole encryption?
-        delta = (1 / lambda_var)
-        e = function_G(h,K,g_t,p,1,delta)
+        e = hash_value_paper(B,u,r,n) # taken only x parts of key_nr => is that the whole encryption?
+        delta = (1 / K)
+        if rate_1 == 'yes':
+            e = function_G(e,K,g_t,p,1,delta)
         #e = np.longdouble(e[0])
         #full_e = u_r * B_prod
         return e
 
     #D(td,h) -- Decoding -- The decoding algorithm takes as input a trapdoor td, a hash value h element of {0,1}**n_long
     #...and outputs a pair of a 0-encoding and a 1-necoding (e0,e1) element of {0,1}**w x {0,1}**w
-    def decoding_algorithm(self,sampling_algorithm,hashing_algorithm,generating_algorithm,encoding_algorithm): #decoding is done by receiver but problem is only needs generator value others are private to sender --> TBD
+    def decoding_algorithm(self,sampling_algorithm,hashing_algorithm,generating_algorithm,encoding_algorithm,rate_1): #decoding is done by receiver but problem is only needs generator value others are private to sender --> TBD
         (G, p, g),A,r = sampling_algorithm #imported for the generator needed but it seams as its not the generator meant but the g from matrix key -- as well receiver can't have access to full Matrix A,otherwise would not need encoding
         g = int(g)
         p = int(p)
@@ -92,25 +95,49 @@ class TDH(object):
         h_s = h**s
         g_t = g**t
         h_s_g_t = h_s*g_t
-        print("h_s_g_t is", h_s_g_t)
-        e_0 = h_s
-        e_1 = h_s_g_t
-        e_0 = function_G(e_0,K,g_t,p,1,delta)
-        e_1 = function_G(e_1,K,g_t,p,1,delta)
-        return e_0, e_1
+        if rate_1 == 'yes':
+            e_0 = h_s
+            e_1 = h_s*g
+            e_0 = function_G(e_0,K,g_t,p,1,delta)
+            e_1 = function_G(e_1,K,g_t,p,1,delta)
+            return e_0, e_1
+        if rate_1 == 'no':
+            print("Rate is",(1 / norm(e)))
+            e_0 = h_s
+            e_1 = h_s_g_t
+            # for i in range(len(h_s)):
+            if e == h_s:
+                x_i = 0
+                print("x**i is", x_i)
+                return e_0, e_1
+            if e == h_s_g_t:
+                x_i = 1
+                print("x**i is", x_i)
+                return e_0, e_1
 
 #aufgeschrieben wegen fehlender Variation, kann aber gar nicht mehr haben, da maximal eine Kolonne/ein e0,e1 für x[i] gefunden werden kann; d.h. nur e0[i],e1[i] per key i ausgeben
         ##4.2.2.Augmentation to rate-1 TDH in the expense of a λ1 error probability
+def main():
+    yes = {'yes', 'y', 'ye'}
+    no = {'no', 'n',''}
 
+    rate_1 = raw_input("Do you want the code to run with rate 1?").lower()
+    if rate_1 in yes:
+        rate_1 = 'yes'
+    elif rate_1 in no:
+        rate_1 = 'no'
+    else:
+        sys.stdout.write("Please enter with either 'yes' or 'no' again")
 
-if __name__ == '__main__':
-    lambda_var = 4                  #int(raw_input("Enter lambda value: "))
-    n = 10                            #int(raw_input("Enter input length: "))
-    sampling_algorithm = TDH(lambda_var,n).sampling_algorithm(lambda_var,n)
-    generating_algorithm = TDH(lambda_var,n).generating_algorithm(sampling_algorithm)
-    hashing_algorithm = TDH(lambda_var,n).hashing_algorithm(sampling_algorithm,n)
-    encoding_algorithm = TDH(lambda_var,n).encoding_algorithm(generating_algorithm,sampling_algorithm,n)
-    decoding_algorithm = TDH(lambda_var,n).decoding_algorithm(sampling_algorithm,hashing_algorithm,generating_algorithm,encoding_algorithm)
-    #print(generating_algorithm[0][1])
+    lambda_var = int(raw_input("Enter lambda value: "))
+    n = int(raw_input("Enter input length: "))
+    sampling_algorithm = TDH(lambda_var,n,rate_1).sampling_algorithm()
+    generating_algorithm = TDH(lambda_var,n,rate_1).generating_algorithm(sampling_algorithm,lambda_var,n)
+    hashing_algorithm = TDH(lambda_var,n,rate_1).hashing_algorithm(sampling_algorithm, n)
+    encoding_algorithm = TDH(lambda_var,n,rate_1).encoding_algorithm(generating_algorithm, sampling_algorithm,n,rate_1)
+    decoding_algorithm = TDH(lambda_var,n,rate_1).decoding_algorithm(sampling_algorithm, hashing_algorithm, generating_algorithm, encoding_algorithm,rate_1)
     print(encoding_algorithm)
     print(decoding_algorithm)
+
+if __name__ == '__main__':
+    main()
